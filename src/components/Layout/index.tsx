f2ae83e20d13c52f35fd54e6ab55218ad362e8fe
araz <araz944memmedov@gmail.com>
2025-02-12 20:30:25 +0400

    Add react router link

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,18 +2,35 @@ import React, { Suspense, useState } from 'react';
 import Sidebar from '../Sidebar';
 import './Layout.css';
 import { useModalStore } from 'stores/modalStore';
+import { useLocation } from "react-router-dom";
 
 const MenuApp = React.lazy(() => import('menu/MenuApp'));
 const Modal = React.lazy(() => import('modal_lib/Modal'));
+
+const titles: Record<string, string> = {
+  "/menu": "Menu",
+  "/tables": "Tables",
+  "/waiters": "Waiters",
+  "/orders": "Orders",
+  "/stats": "Stats",
+  "/settings": "Settings",
+};
 
 const Layout = () => {
   const { isModalOpen } = useModalStore((state: any) => state);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  const location = useLocation();
+    const title = titles[location.pathname] || "Not found";
+
   return (
     <>
       {isModalOpen && (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense
+          fallback={
+            <div className="spinner"></div>
+          }
+        >
           <Modal />
         </Suspense>
       )}
@@ -22,12 +39,16 @@ const Layout = () => {
         <div className="custom-layout-child">
           <div className="custom-component-container">
             <div className="custom-section-title">
-              <div className="custom-title-background">Menu</div>
+              <div className="custom-title-background">{title}</div>
             </div>
             <div className="custom-horizontal-linear"></div>
             <div className="custom-component-section">
               <div className="custom-component-background">
-                <Suspense fallback={<div>Loading...</div>}>
+                <Suspense
+                  fallback={
+                    <div className="spinner"></div>
+                  }
+                >
                   <MenuApp />
                 </Suspense>
               </div>
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,22 +1,59 @@
 import React from 'react';
 import './Sidebar.css';
-import arrowIcon from "assets/svg/arrow-square-left.svg";
+import arrowIcon from 'assets/svg/arrow-square-left.svg';
+import menuIcon from 'assets/svg/menu.svg';
+import tablesIcon from 'assets/svg/tables.svg';
+import waitersIcon from 'assets/svg/waiters.svg';
+import ordersIcon from 'assets/svg/orders.svg';
+import statsIcon from 'assets/svg/stats.svg';
+import settingsIcon from 'assets/svg/settings.svg';
+import { Link } from 'react-router-dom';
 
 const Sidebar = ({ isOpen, toggleSidebar }: any) => {
   return (
     <>
-      <div className={`custom-sidebar ${!isOpen ? 'w-19vw' : 'w-5vw'}`}>
+      <div className={`custom-sidebar ${!isOpen ? 'custom-sidebar-open' : 'w-5vw'}`}>
         <div className="custom-logo-container">Restaurant Demo</div>
         <ul className="custom-ul">
-          <li className="custom-list-element">Menu</li>
-          <li className="custom-list-element">Tables</li>
-          <li className="custom-list-element">Waiters</li>
-          <li className="custom-list-element">Orders</li>
-          <li className="custom-list-element">Stats</li>
-          <li className="custom-list-element">Settings</li>
+          <Link to="/menu">
+            <li className="custom-list-element">
+              <img className="custom-list-icon" src={menuIcon} alt="menu" />
+              Menu
+            </li>
+          </Link>
+          <Link to="/tables">
+            <li className="custom-list-element">
+              <img className="custom-list-icon" src={tablesIcon} alt="tables" />
+              Tables
+            </li>
+          </Link>
+          <Link to="/waiters">
+            <li className="custom-list-element">
+              <img className="custom-list-icon" src={waitersIcon} alt="waiters" />
+              Waiters
+            </li>
+          </Link>
+          <Link to="/orders">
+            <li className="custom-list-element">
+              <img className="custom-list-icon" src={ordersIcon} alt="orders" />
+              Orders
+            </li>
+          </Link>
+          <Link to="/stats">
+            <li className="custom-list-element">
+              <img className="custom-list-icon" src={statsIcon} alt="stats" />
+              Stats
+            </li>
+          </Link>
+          <Link to="/settings">
+            <li className="custom-list-element">
+              <img className="custom-list-icon" src={settingsIcon} alt="settings" />
+              Settings
+            </li>
+          </Link>
         </ul>
-        <button className={`custom-toggle-button ${!isOpen ? "left-15vw" : "left-4vw"}`} onClick={toggleSidebar}>
-          <img className={`custom-button-icon ${!isOpen ? "" : "flip-x"}`} src={arrowIcon} alt="arrow" />
+        <button className={`custom-toggle-button ${!isOpen ? 'left-15vw' : 'left-4vw'}`} onClick={toggleSidebar}>
+          <img className={`custom-button-icon ${!isOpen ? '' : 'flip-x'}`} src={arrowIcon} alt="arrow" />
         </button>
       </div>
     </>
