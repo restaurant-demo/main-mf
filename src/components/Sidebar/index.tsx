34f55f8a9f889624dce6def9630c28c2a0120757
araz <araz944memmedov@gmail.com>
2025-02-14 13:28:04 +0400

    Make global variable for NavLink classNames

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -7,53 +7,79 @@ import waitersIcon from 'assets/svg/waiters.svg';
 import ordersIcon from 'assets/svg/orders.svg';
 import statsIcon from 'assets/svg/stats.svg';
 import settingsIcon from 'assets/svg/settings.svg';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = ({ isOpen, toggleSidebar }: any) => {
+  const linkClass = ({ isActive }: { isActive: boolean }) =>
+    `custom-router-link ${isActive ? `custom-active-link ${isOpen ? 'custom-translateX-open' : 'custom-translateX-close'}` : 'custom-inactive-link'}`;
+  const linkStyle = {
+    padding: isOpen ? '0.76vw 0 0.76vw 1.2vw' : '0.76vw 0'
+  };
+
   return (
     <>
-      <div className={`custom-sidebar ${!isOpen ? 'custom-sidebar-open' : 'w-5vw'}`}>
-        <div className="custom-logo-container">Restaurant Demo</div>
+      <div className={`custom-sidebar ${isOpen ? 'custom-sidebar-open' : 'custom-sidebar-close'}`}>
+        <div className={`custom-logo-container ${isOpen ? 'opacity-100' : 'opacity-0'}`}>Restaurant Demo</div>
         <ul className="custom-ul">
-          <Link to="/menu">
+          <NavLink to="/menu" className={linkClass} style={linkStyle}>
             <li className="custom-list-element">
-              <img className="custom-list-icon" src={menuIcon} alt="menu" />
-              Menu
+              <img className={`custom-list-icon`} src={menuIcon} alt="menu" />
+              <span className={`custom-navlink-text ${isOpen ? 'opacity-100' : 'opacity-0'}`}>Menu</span>
             </li>
-          </Link>
-          <Link to="/tables">
+          </NavLink>
+          <NavLink to="/tables" className={linkClass} style={linkStyle}>
             <li className="custom-list-element">
-              <img className="custom-list-icon" src={tablesIcon} alt="tables" />
-              Tables
+              <img
+                className={`custom-list-icon`}
+                src={tablesIcon}
+                alt="tables"
+              />
+              <span className={`custom-navlink-text ${isOpen ? 'opacity-100' : 'opacity-0'}`}>Tables</span>
             </li>
-          </Link>
-          <Link to="/waiters">
+          </NavLink>
+          <NavLink to="/waiters" className={linkClass} style={linkStyle}>
             <li className="custom-list-element">
-              <img className="custom-list-icon" src={waitersIcon} alt="waiters" />
-              Waiters
+              <img
+                className={`custom-list-icon`}
+                src={waitersIcon}
+                alt="waiters"
+              />
+              <span className={`custom-navlink-text ${isOpen ? 'opacity-100' : 'opacity-0'}`}>Waiters</span>
             </li>
-          </Link>
-          <Link to="/orders">
+          </NavLink>
+          <NavLink to="/orders" className={linkClass} style={linkStyle}>
             <li className="custom-list-element">
-              <img className="custom-list-icon" src={ordersIcon} alt="orders" />
-              Orders
+              <img
+                className={`custom-list-icon`}
+                src={ordersIcon}
+                alt="orders"
+              />
+              <span className={`custom-navlink-text ${isOpen ? 'opacity-100' : 'opacity-0'}`}>Orders</span>
             </li>
-          </Link>
-          <Link to="/stats">
+          </NavLink>
+          <NavLink to="/stats" className={linkClass} style={linkStyle}>
             <li className="custom-list-element">
-              <img className="custom-list-icon" src={statsIcon} alt="stats" />
-              Stats
+              <img
+                className={`custom-list-icon`}
+                src={statsIcon}
+                alt="stats"
+              />
+              <span className={`custom-navlink-text ${isOpen ? 'opacity-100' : 'opacity-0'}`}>Stats</span>
             </li>
-          </Link>
-          <Link to="/settings">
+          </NavLink>
+          <NavLink to="/settings" className={linkClass} style={linkStyle}>
             <li className="custom-list-element">
-              <img className="custom-list-icon" src={settingsIcon} alt="settings" />
-              Settings
+              <img
+                className={`custom-list-icon`}
+                src={settingsIcon}
+                alt="settings"
+              />
+              <span className={`custom-navlink-text ${isOpen ? 'opacity-100' : 'opacity-0'}`}>Settings</span>
             </li>
-          </Link>
+          </NavLink>
         </ul>
-        <button className={`custom-toggle-button ${!isOpen ? 'left-15vw' : 'left-4vw'}`} onClick={toggleSidebar}>
-          <img className={`custom-button-icon ${!isOpen ? '' : 'flip-x'}`} src={arrowIcon} alt="arrow" />
+        <button className={`custom-toggle-button ${isOpen ? 'left-15vw' : 'left-4vw'}`} onClick={toggleSidebar}>
+          <img className={`custom-button-icon ${isOpen ? '' : 'flip-x'}`} src={arrowIcon} alt="arrow" />
         </button>
       </div>
     </>
